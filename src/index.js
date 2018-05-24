221ea95091a8efcd22986c1031c9e532e5d77c28
RedTn <redmond.tran@gmail.com>
2018-05-23 23:20:27 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import storeFactory from './store'
+import { suggestResortNames } from './actions'
 
-ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line react/jsx-filename-extension, no-undef
-registerServiceWorker();
+const store = storeFactory()
+
+store.dispatch(
+	suggestResortNames("sq")
+)
